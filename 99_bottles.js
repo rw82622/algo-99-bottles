3a30f44741535b84334e7be5f1e079a227399c88
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-23 16:58:11 -0500

    solved challenge using recursion

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,21 +1,25 @@
 function bottleSong(number) {
   // Write your code here!
-  for (let num = number; num >= 0; num--) {
-    if (num >= 1) {
+  let num = number;
+  function bottle(number) {
+    if (number == 0) {
+      return console.log(`No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, ${num} bottle${
+        number > 1 ? "s" : ""
+      } of beer on the wall.`);
+    } else {
       console.log(
-        `${num} bottle${num > 1 ? "s" : ""} of beer on the wall, ${num} bottle${
-          num > 1 ? "s" : ""
-        } of beer.
-Take one down and pass it around, ${num - 1 >= 1 ? num - 1 : "no more"} bottle${
-          num - 1 == 1 ? "" : "s"
-        } of beer on the wall.        `
+        `${number} bottle${
+          number > 1 ? "s" : ""
+        } of beer on the wall, ${number} bottle${number > 1 ? "s" : ""} of beer.
+Take one down and pass it around, ${
+          number - 1 >= 1 ? number - 1 : "no more"
+        } bottle${number - 1 == 1 ? "" : "s"} of beer on the wall.        `
       );
+      return bottle(number - 1);
     }
   }
-  console.log(`No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, ${number} bottle${
-    number > 1 ? "s" : ""
-  } of beer on the wall.`);
+  bottle(number);
 }
 
 bottleSong(3);
