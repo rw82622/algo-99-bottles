e34073e508805aad6bce8d94db58f40f1b62b89b
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-23 15:59:05 -0500

    solved challenge using a for loop

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,6 +1,21 @@
-function bottleSong() {
+function bottleSong(number) {
   // Write your code here!
-  console.log("Hi");
+  for (let num = number; num >= 0; num--) {
+    if (num >= 1) {
+      console.log(
+        `${num} bottle${num > 1 ? "s" : ""} of beer on the wall, ${num} bottle${
+          num > 1 ? "s" : ""
+        } of beer.
+Take one down and pass it around, ${num - 1 >= 1 ? num - 1 : "no more"} bottle${
+          num - 1 == 1 ? "" : "s"
+        } of beer on the wall.        `
+      );
+    }
+  }
+  console.log(`No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, ${number} bottle${
+    number > 1 ? "s" : ""
+  } of beer on the wall.`);
 }
 
-bottleSong();
+bottleSong(3);
